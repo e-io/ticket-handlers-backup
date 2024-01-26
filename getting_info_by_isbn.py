a2dac1cd8846252e1cd1f1327fec7417ae08ff7c
Pavel <pmikhailov@wiley.com>
2024-01-26 11:34:59 +0200

    print --> logger and another code style enhancements.

--- getting_info_by_isbn.py
+++ getting_info_by_isbn.py
@@ -2,7 +2,7 @@
 This script returns information about a book by its ISBN with Google Books API.
 How to run.
 Example:
-python3 getting_info_by_isbn.py 978-1-119-70711-0
+python3 getting_info_by_isbn.py 978-1-119-70711-0 9781394186921 9781394182930 9781119909378
 general view:
 <python> <path/to/script.py> <isbn1> <isbn2> ... <isbn100500>
 example of isbn: "978-1-119-70711-0". It can be 10 digits or 13 digits. It can be with hyphens or without them.
@@ -21,35 +21,18 @@ from text_to_num import alpha2digit
 
 from logger import logger
 
-# read_from = Path("Prioritized_products_containing_issues.tsv")
-
 logger.setLevel(level='INFO')
+
 
 class Book:
     def __init__(self):
         pass
 
     def print_info(self):
-        info = self.__dict__
         print()
+        info = self.__dict__
         pprint(info, sort_dicts=False, underscore_numbers=True)
         print()
-
-        """
-        print("ISBN:", '\n\t', isbn, sep='')
-        print("Authors:")
-        for author in book.authors:
-            print('\t', author)
-        print("Title:")
-        print('\t', title)
-        print("Published on:")
-        print('\t', publishedDate)
-        print("Short description:", '\n\t', short_description, sep='')
-        print("Number of pages:", '\n\t', pages, sep='')
-
-        for item, data in info_to_print:
-            print(item, '\n\t', data, sep='')
-        """
 
     def create_folder_name(self):
         """
@@ -66,7 +49,7 @@ class Book:
 
 
 def get_info_by_isbn(isbn: str):
-    url = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbn
+    url = 'https://www.googleapis.com/books/v1/volumes?q=isbn:' + isbn
 
     response = requests.get(url)
 
@@ -75,17 +58,20 @@ def get_info_by_isbn(isbn: str):
 
     json_all = response.json()
     if json_all['totalItems'] == 0:
-        print("There is no information on server about book with ISBN", isbn, ". \nBook is skipped.")
+        logger.warning('There is no information on server about book with ISBN %s. \nBook is skipped.', isbn)
         return 0
-    elif json_all['totalItems'] > 1:
-        print("Book with ISBN", isbn, "has several 'items'. \nBook is skipped.")
-        return 0
+
+    if json_all['totalItems'] > 1:
+        logger.warning("Book with ISBN %s has several 'items'. Book is not skipped. \n"
+                       "First item it taken, because usually items are identical.\n", isbn)
+        # logger.debug('\n-------\n%s\n-------\n', json.dumps(json_all['items'], indent=2))  # to show all items inside
+        # return 0
 
     data_all = json_all['items'][0]
     info = data_all['volumeInfo']
 
     # print all json
-    print("-------\n", json.dumps(data_all, indent=2), "\n-------\n", sep='', end='')
+    logger.debug('\n-------\n%s\n-------\n', json.dumps(data_all, indent=2))
 
     book = Book()
 
@@ -95,51 +81,56 @@ def get_info_by_isbn(isbn: str):
     book.isbn = isbn  # or info['industryIdentifiers'][0 <or> 1]['identifier']
     book.authors = info['authors']
     book.title = info['title']
-    book.subtitle = info['subtitle'] if 'subtitle' in info else None
+    book.subtitle = add_field(info, 'subtitle')
     book.publisher = info['publisher']
 
     description = (info['description'] + ' ' + data_all['searchInfo']['textSnippet']).lower().replace('canadian', '')
-    print(description)
-    match = re.search("([A-Za-z0-9_-]*)\s+edition", description)
+    logger.debug(description)
+
+    # ------Searching edition----------
+    match = re.search('([A-Za-z0-9_-]*)\s+edition', description)
     book.edition = match.group(1).lower() if match is not None else None
-    print('edition is ', book.edition)
+    logger.debug('edition is %s', book.edition)
     if book.edition is not None:
-        print(book.edition)
+        logger.debug(book.edition)
         if book.edition == 'second':
             book.edition = 2
         elif book.edition == 'third':
             book.edition = 3
         else:
             book.edition = alpha2digit(book.edition, 'en')
-            match_num = re.search("([0-9]*)", book.edition)
+            match_num = re.search('([0-9]*)', book.edition)
             if match_num is not None:
                 num = match_num.group(1)
                 if len(num) != 0:
                     book.edition = int(num)
                 else:
                     book.edition = None
+    # ---------------------
 
     book.category = info['categories'][0]
     book.published_date = info['publishedDate']
-    book.page_count = add_field(info, 'pageCount')
+    if 'pageCount' in info and info['pageCount'] > 0:
+        book.page_count = info['pageCount']
+    else:
+        book.page_count = add_field(info, 'printedPageCount')
     book.text_snippet = data_all['searchInfo']['textSnippet']
 
-    book.thumbnail = info["imageLinks"]['thumbnail']
-    book.info_link = info["infoLink"]
+    book.thumbnail = info['imageLinks']['thumbnail']
+    book.info_link = info['infoLink']
+    book.full_json = data_all['selfLink']
 
     book.rating = add_field(info, 'averageRating')
     book.ratings_count = add_field(info, 'ratingsCount')
     book.create_folder_name()
 
-    book.print_info()
+    if logger.level == 'DEBUG':
+        book.print_info()
 
     return book
 
 
-def main(args: list):
-    with ThreadPoolExecutor() as pool:
-        results = pool.map(get_info_by_isbn, args)
-
+def create_tsv(results: list):
     books = []
     for result in results:
         if result != 0:
@@ -150,18 +141,50 @@ def main(args: list):
 
     with open(path, 'w+') as file:
         writer = csv.writer(file, delimiter='\t')
-        header = ['isbn', 'published', 'edition',
-                  'pages', 'publisher', 'authors', 'title',
-                  'subtitle', 'category', 'info link']
+        header = [
+            'isbn',
+            'published',
+            'edition',
+            'pages',
+            'publisher',
+            'authors',
+            'title',
+            'subtitle',
+            'category',
+            'full JSON',
+            'info link',
+        ]
 
         writer.writerow(header)
         for book in books:
-            row = [book.isbn, book.published_date, book.edition,
-                   book.page_count, book.publisher, ', '.join(book.authors), book.title,
-                   book.subtitle, book.category, book.info_link]
+            row = [
+                book.isbn,
+                book.published_date,
+                book.edition,
+                book.page_count,
+                book.publisher,
+                ', '.join(book.authors),
+                book.title,
+                book.subtitle,
+                book.category,
+                book.full_json,
+                book.info_link,
+            ]
             writer.writerow(row)
-    logger.debug("Work completed")
 
 
-if __name__ == "__main__":
+def main(args: list):
+    logger.info('Work started')
+
+    with ThreadPoolExecutor() as pool:
+        results = pool.map(get_info_by_isbn, args)
+
+    create_tsv(results)
+
+    logger.info('Work completed')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        raise Exception("Not enough arguments. Pass ISBN(s) as arguments.")
     main(sys.argv[1:])
